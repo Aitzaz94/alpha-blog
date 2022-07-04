9a112dbcd52da51f4e5ab5161e683fb5391b01f6
Aitzaz94 <aitzazakmal@gmail.com>
2022-07-04 18:03:18 +0500

    edit, update,show, index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,53 @@
 class ArticlesController < ApplicationController
+
+    def index
+        @articles = Article.all
+    end
+    
+
     def new
         @article = Article.new
-    end    
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
 
     def create
         # render plain: params[:article].inspect
         @article = Article.new(article_params)
         if @article.save
-            redirect_to articles_show(@article)
+            flash[:notice] = "Article created successfully!"
+            redirect_to article_path(@article)
+        else
+            flash[:notice] = @article.errors.full_messages
+            render :new
         end
     end
     
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+          flash[:success] = "Article was successfully updated"
+          redirect_to article_path(@article)
+        else
+        #   flash[:error] = "Something went wrong"
+          render :edit
+        end
+    end
+    
+
+    def show
+
+        @article = Article.find(params[:id])
+        
+    end
+
     private
 
     def article_params
         params.require(:article).permit(:title, :description)
     end
-
+    
 
 end
