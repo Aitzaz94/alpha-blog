89d590c57e3a24d59a2d793913a5f904b15bea88
Aitzaz94 <aitzazakmal@gmail.com>
2022-07-05 16:52:39 +0500

    association between articles, users and debugger view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,12 @@
 class Article < ApplicationRecord
     
+    belongs_to :user
+    
     validates :title, presence: true, length: {minimum:5, maximum:60}
     validates :description, presence: true, length: {minimum:25, maximum:250}
+    validates :user_id, presence: true
+    
+    
+    
 
 end
